114cc18f3c1aafb6efeca115da30d23af5e213c8
kirahan <hanzhaoapply@gmail.com>
2014-04-30 01:44:02 +0200

    multi-channel with 2 bug p1.2 and p1.4 can't read value

--- main.c
+++ main.c
@@ -4,12 +4,12 @@ void store_singlechanel(int *register_address,int *flag, int flag_turn );
 unsigned int *ADC_Result;
 unsigned int count=0;
 
-int flag_red=0;
-int flag_yellow=0;
-int flag_green=0;
-int flag_white=0;
-int flag_orange=0;
-int flag_blue=0;
+int flag_red=1;
+int flag_yellow=1;
+int flag_green=1;
+int flag_white=1;
+int flag_orange=1;
+int flag_blue=1;
 
 #define FRAM_TEST_START 0xD000
 #define turn_red 0x2000
@@ -28,17 +28,23 @@ int main(void)
 
 
 	    //set ADC input ports
-	    //P1.2 for  'red'
-		//P1.3 for 	'yellow'
-		//P1.4 for 	'green'
-		//P1.5 for 	'white'
-		//P1.6 for 	'orange'
-		//P1.7 for 	'blue'
-	      P1SEL0 |= BIT3 ;
-	      P1SEL1 |= BIT3 ;
-	      //	      P1SEL0 |= BIT2 | BIT3 | BIT4 | BIT5 | BIT6 | BIT7;
-	      //	      P1SEL1 |= BIT2 | BIT3 | BIT4 | BIT5 | BIT6 | BIT7;
+	    //P1.2 for  'red'     	A2
+		//P1.3 for 	'yellow'	A3
+		//P1.4 for 	'green'		A4
+		//P1.5 for 	'white'		A5
+		//P2.4 for 	'orange'	A7
+		//P4.3 for 	'blue'		A11
+//	        P1SEL0 |= BIT3 ;
+//	        P1SEL1 |= BIT3 ;
 
+
+		  P1SEL0 |= BIT2 | BIT3 | BIT4 | BIT5 ;
+		  P1SEL1 |= BIT2 | BIT3 | BIT4 | BIT5 ;
+	      P2SEL0 |=	BIT3 ;
+		  P2SEL1 |= BIT3 ;
+
+		  P4SEL0 |=	BIT3 ;
+		  P4SEL1 |= BIT3 ;
 	      // Disable the GPIO power-on default high-impedance mode to activate
 	      // previously configured port settings
 	      PM5CTL0 &= ~LOCKLPM5;
@@ -48,17 +54,21 @@ int main(void)
 
 	      // Configure ADC12
 	      ADC12CTL0 = ADC12SHT0_2 | ADC12ON;         			// 16 click cycle for sample-and-hold time.
-	      ADC12CTL1 = ADC12SHP | ADC12PDIV_3 |ADC12DIV_7 | ADC12SSEL_2; 		// SAMPON form the sampling timer, ADC predivider by 64, clock divider by 8, CLOCK SOURCE SELECT MCLK= 1MHZ
+	      ADC12CTL1 = ADC12SHP | ADC12PDIV_3 |ADC12DIV_7 | ADC12SSEL_2  | ADC12CONSEQ_1; 		// SAMPON form the sampling timer, ADC predivider by 64, clock divider by 8, CLOCK SOURCE SELECT MCLK= 1MHZ
 	      ADC12CTL2 |= ADC12RES_2;                  // 12-bit conversion results
-	      ADC12IER0 |= ADC12IE0;
-	      //	      ADC12IER0 |= ADC12IE0 | ADC12IE1 | ADC12IE2 | ADC12IE3 | ADC12IE4 | ADC12IE5;                    // Enable ADC conv complete interrupts
-	      ADC12MCTL0 |= ADC12INCH_3 | ADC12VRSEL_0; // A3 ADC input select; 0-3.3v
-	      //	      ADC12MCTL0 |= ADC12INCH_2 | ADC12VRSEL_0; // A2 ADC input select; 0-3.3v
-	      //	      ADC12MCTL1 |= ADC12INCH_3 | ADC12VRSEL_0; // A3 ADC input select; 0-3.3v
-	      //	      ADC12MCTL2 |= ADC12INCH_4 | ADC12VRSEL_0; // A4 ADC input select; 0-3.3v
-	      //	      ADC12MCTL3 |= ADC12INCH_5 | ADC12VRSEL_0; // A5 ADC input select; 0-3.3v
-	      //	      ADC12MCTL4 |= ADC12INCH_6 | ADC12VRSEL_0; // A6 ADC input select; 0-3.3v
-	      //	      ADC12MCTL5 |= ADC12INCH_7 | ADC12VRSEL_0; // A7 ADC input select; 0-3.3v
+	      ADC12CTL3 |= ADC12CSTARTADD_0;
+
+	      ADC12IER0 |= ADC12IE0 | ADC12IE1 | ADC12IE2 | ADC12IE3 | ADC12IE4 | ADC12IE5;                    // Enable ADC conv complete interrupts
+	      	      ADC12MCTL0 |= ADC12INCH_2 | ADC12VRSEL_0; // A2 ADC input select; 0-3.3v
+	      	      ADC12MCTL1 |= ADC12INCH_3 | ADC12VRSEL_0; // A3 ADC input select; 0-3.3v
+	      	      ADC12MCTL2 |= ADC12INCH_4 | ADC12VRSEL_0; // A12 ADC input select; 0-3.3v
+	      	      ADC12MCTL3 |= ADC12INCH_5 | ADC12VRSEL_0; // A5 ADC input select; 0-3.3v
+	      	      ADC12MCTL4 |= ADC12INCH_7 | ADC12VRSEL_0 ; // A7 ADC input select; 0-3.3v
+	      	       ADC12MCTL5 |= ADC12INCH_11 | ADC12VRSEL_0 | ADC12EOS; // A11 ADC input select; 0-3.3v
+
+	      	      //	      	      ADC12IER0 |= ADC12IE1;
+	      	      //ADC12MCTL2 |= ADC12INCH_3 | ADC12VRSEL_0 | ADC12EOS; // A3 ADC input select; 0-3.3v
+
 	      ADC_Result= (unsigned int *)FRAM_TEST_START;
 
 
@@ -90,20 +100,26 @@ __interrupt void ADC12_ISR(void)
     case 12:								//red facet
     	store_singlechanel((int *)&ADC12MEM0,(int *)&flag_red, turn_red );
     	__bic_SR_register_on_exit(LPM0_bits); // Exit active CPU
-
       break;                                // Clear CPUOFF bit from 0(SR)
     case 14:								//yellow facet
+    	store_singlechanel((int *)&ADC12MEM1,(int *)&flag_yellow, turn_yellow );
+    	__bic_SR_register_on_exit(LPM0_bits); // Exit active CPU
     	break;                         // Vector 14:  ADC12MEM1
     case 16:							//green facet
+    	store_singlechanel((int *)&ADC12MEM2,(int *)&flag_green, turn_green );
+    	__bic_SR_register_on_exit(LPM0_bits); // Exit active CPU
     	break;                          // Vector 16:  ADC12MEM2
     case 18:							//white facet
-
+    	store_singlechanel((int *)&ADC12MEM3,(int *)&flag_white, turn_white );
+    	__bic_SR_register_on_exit(LPM0_bits); // Exit active CPU
     	break;                         // Vector 18:  ADC12MEM3
     case 20:							//orange facet
-
+    	store_singlechanel((int *)&ADC12MEM4,(int *)&flag_orange, turn_orange );
+    	__bic_SR_register_on_exit(LPM0_bits); // Exit active CPU
     	break;                         // Vector 20:  ADC12MEM4
     case 22:							//blue facet
-
+    	store_singlechanel((int *)&ADC12MEM5,(int *)&flag_blue, turn_blue );
+    	__bic_SR_register_on_exit(LPM0_bits); // Exit active CPU
     	break;                         // Vector 22:  ADC12MEM5
     case 24: break;                         // Vector 24:  ADC12MEM6
     case 26: break;                         // Vector 26:  ADC12MEM7
